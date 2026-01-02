3eb7cb406b907adffc2f9f7ea3c7624318cd8aa6
AlketaThacii <alketa.thaci@student.uni-pr.edu>
2026-01-02 19:25:38 +0100

    Shtimi i butonit "Back to Top" per te gjitha faqet

--- javascript/visitUs.js
+++ javascript/visitUs.js
@@ -82,3 +82,21 @@ $(document).ready(function () {
         lastScrollTop = st;
     });
 });
+//Funksioni per butonin "Back to Top"
+$(document).ready(function () {
+    $('body').append('<div id="backToTop">&#8679;</div>');
+
+    var backToTop = $('#backToTop');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            backToTop.fadeIn(300);
+        } else {
+            backToTop.fadeOut(300);
+        }
+    });
+    backToTop.click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+});
